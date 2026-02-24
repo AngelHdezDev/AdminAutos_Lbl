d6a61eb9b81e1b4c23a33e51a327aa119eb3f979
AngelHdezDev <angeldev20340@gmail.com>
2026-02-24 14:12:39 -0600

    Nueva tabla de imagen_temporals

---
+++ app/Models/ImagenTemporal.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ImagenTemporal extends Model
+{
+    protected $fillable = [
+        'ruta_archivo',
+        'nombre_original',
+        'correo_origen',
+        'asunto',
+        'fecha_correo'
+    ];
+}
